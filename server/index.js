d39cf67cfb5d0899e5c887c0b79f8d308279dd87
Sean Gowans <sean@Seans-MBP.fritz.box>
2025-11-12 01:01:01 +0100

    Render Fix

--- server/index.js
+++ server/index.js
@@ -1,20 +1,64 @@
+// import express from 'express';
+// import morgan from 'morgan';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+// import 'dotenv/config';
+// import projectRoutes from './routes/project.js';
+// import userRoutes from './routes/user.js';
+// import authRoutes from './routes/auth.js';
+// import contactRoutes from "./routes/contact.js";
+// import educationRoutes from './routes/education.js';
+
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((err) => console.error('MongoDB connection error:', err));
+
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use(morgan('dev'));
+
+// app.use('/api/education', educationRoutes);
+// app.use("/api/contact", contactRoutes);
+// app.use('/api/projects', projectRoutes);
+// app.use('/api/users', userRoutes);
+// app.use('/auth', authRoutes);
+
+// app.use('/api/data', (req, res) => {
+//   res.json({ message: 'Hello from the API! Again' });
+// });
+
+
+// const port = process.env.PORT || 4000;
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
+
+
+
+
+
 import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import projectRoutes from './routes/project.js';
 import userRoutes from './routes/user.js';
 import authRoutes from './routes/auth.js';
 import contactRoutes from "./routes/contact.js";
 import educationRoutes from './routes/education.js';
 
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
-
 
 const app = express();
 app.use(cors());
@@ -28,11 +72,20 @@ app.use('/api/users', userRoutes);
 app.use('/auth', authRoutes);
 
 app.use('/api/data', (req, res) => {
-  res.json({ message: 'Hello from the API! Again' });
+  res.json({ message: 'Hello from the API!' });
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const clientDistPath = path.join(__dirname, '../client/dist');
+
+app.use(express.static(clientDistPath));
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(clientDistPath, 'index.html'));
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`🚀 Server running at http://localhost:${port}/`);
 });
